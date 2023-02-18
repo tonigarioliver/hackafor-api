930d7aa4f245e895248285cae8a89d0e88e647f3
tonigarioliver <tonietmoscari@gmail.com>
2023-02-18 19:23:06 +0100

    name changed

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def index() -> RedirectResponse:
     return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")

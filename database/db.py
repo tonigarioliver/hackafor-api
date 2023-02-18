4b3dbef5a9378894ef837ebdf5c4e84979aa55c0
tonigarioliver <tonietmoscari@gmail.com>
2023-02-18 21:25:37 +0100

    update bd details

--- database/db.py
+++ database/db.py
@@ -7,8 +7,11 @@ from supabase import create_client
 load_dotenv()
 
 
-URL: str = os.environ.get("SUPABASE_URL")
-KEY: str = os.environ.get("SUPABASE_KEY")
+#URL: str = os.environ.get("SUPABASE_URL")
+#KEY: str = os.environ.get("SUPABASE_KEY")
+URL: str="https://aoombgglyswwhkqniife.supabase.co"
+KEY: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvb21iZ2dseXN3d2hrcW5paWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0MDE0NjksImV4cCI6MTk5MTk3NzQ2OX0.g_qldX2hxVzPkcXbBAUf5MW-qEnnOU6M4YuL4TWPpr8"
+
 
 class DB:
 

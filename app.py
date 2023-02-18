dd39449dbaf6fb627a375abeceec76474cb02dc5
tonigarioliver <tonietmoscari@gmail.com>
2023-02-18 19:16:36 +0100

    second try

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.products import products_router
 from starlette.responses import RedirectResponse
+import uvicorn
+import os
 
 
 app = FastAPI()
@@ -11,3 +13,6 @@ app.include_router(products_router)
 @app.get("/")
 def index() -> RedirectResponse:
     return RedirectResponse(url="/docs")
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
